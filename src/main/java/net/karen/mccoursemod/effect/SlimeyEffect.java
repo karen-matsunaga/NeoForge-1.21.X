5cf011099055d49de93121a1817e361dd5d89acc
karen matsunaga <Karen.f.matsunaga@gmail.com>
2025-08-06 19:33:59 -0300

    2025-08-06 Improved Slimey effect comments and codes

--- src/main/java/net/karen/mccoursemod/effect/SlimeyEffect.java
+++ src/main/java/net/karen/mccoursemod/effect/SlimeyEffect.java
@@ -1,10 +1,12 @@
 package net.karen.mccoursemod.effect;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
 
@@ -16,20 +18,21 @@ public class SlimeyEffect extends MobEffect {
     }
 
     @Override
-    public boolean applyEffectTick(@NotNull ServerLevel level, @NotNull LivingEntity livingEntity, int amplifier) {
-        if (livingEntity instanceof Player player) {
-            if (player.horizontalCollision) {
-                Vec3 initialVec = player.getDeltaMovement();
+    public boolean applyEffectTick(@NotNull ServerLevel serverLevel, @NotNull LivingEntity livingEntity, int amplifier) {
+        Level level = livingEntity.level();
+        BlockPos pos = livingEntity.blockPosition();
+        BlockState state = level.getBlockState(pos);
+        if (state.isCollisionShapeFullBlock(level, pos)) {
+            if (livingEntity.horizontalCollision || livingEntity.onClimbable()) {
+                Vec3 initialVec = livingEntity.getDeltaMovement();
                 Vec3 climbVec = new Vec3(initialVec.x, 0.2D, initialVec.z);
-                player.setDeltaMovement(climbVec.scale(0.96D));
+                livingEntity.setDeltaMovement(climbVec.scale(0.96D));
                 return true;
             }
         }
-        return super.applyEffectTick(level, livingEntity, amplifier);
+        return super.applyEffectTick(serverLevel, livingEntity, amplifier);
     }
 
     @Override
-    public boolean shouldApplyEffectTickThisTick(int duration, int amplifier) {
-        return true;
-    }
+    public boolean shouldApplyEffectTickThisTick(int duration, int amplifier) { return true; }
 }
