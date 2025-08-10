e28e69192270b6faccf78f4f09d9f276ad1c136a
karen matsunaga <Karen.f.matsunaga@gmail.com>
2025-08-10 14:23:20 -0300

    2025-08-10 Improved Image Tooltip Component comments and codes

--- src/main/java/net/karen/mccoursemod/util/ImageTooltipComponent.java
+++ src/main/java/net/karen/mccoursemod/util/ImageTooltipComponent.java
@@ -3,6 +3,7 @@ package net.karen.mccoursemod.util;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipComponent;
+import net.minecraft.client.resources.language.I18n;
 import net.minecraft.world.inventory.tooltip.TooltipComponent;
 import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
@@ -20,11 +21,17 @@ public class ImageTooltipComponent implements ClientTooltipComponent, TooltipCom
     }
 
     @Override
+    public void renderText(@NotNull GuiGraphics graphics, @NotNull Font font, int x, int y) {
+        graphics.drawString(font, I18n.get("tooltip.mccoursemod.more_ores.tooltip"), x, y, 0xFFFFFF, true);
+        graphics.renderItemDecorations(font, item, x + 95, y - 4, text); // Render TEXT
+    }
+
+    @Override
     public void renderImage(@NotNull Font font, int x, int y, int width, int height, @NotNull GuiGraphics graphics) {
         // Render Image -> Example: Icon 8x8, Icon 9x9, Icon 16x16, etc. (width x height)
         graphics.renderItem(item, x, y); // Render TEXTURE ITEM
         // Render Text -> Example: [ICON] text...
-        graphics.renderItemDecorations(font, item, x + 95, y - 4, text); // Render TEXT
+        renderText(graphics, font, x, y);
     }
 
     // Image height
