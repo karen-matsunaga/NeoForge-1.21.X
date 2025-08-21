ba4e0ffb275add7b26909fc3fad5ab3521aa031d
karen matsunaga <Karen.f.matsunaga@gmail.com>
2025-08-21 11:27:58 -0300

    2025-08-21 Fixed Tomahawk render projectile item

---
+++ src/main/java/net/karen/mccoursemod/entity/client/TomahawkProjectileRenderState.java
@@ -0,0 +1,8 @@
+package net.karen.mccoursemod.entity.client;
+
+import net.minecraft.client.renderer.entity.state.EntityRenderState;
+
+public class TomahawkProjectileRenderState extends EntityRenderState {
+    public float xRot;
+    public float yRot;
+}
--- src/main/java/net/karen/mccoursemod/entity/client/TomahawkProjectileRenderer.java
+++ src/main/java/net/karen/mccoursemod/entity/client/TomahawkProjectileRenderer.java
@@ -2,19 +2,23 @@ package net.karen.mccoursemod.entity.client;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Axis;
 import net.karen.mccoursemod.MccourseMod;
 import net.karen.mccoursemod.entity.custom.TomahawkProjectileEntity;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.ItemRenderer;
-import net.minecraft.client.renderer.entity.state.EntityRenderState;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
 import org.jetbrains.annotations.NotNull;
 
-public class TomahawkProjectileRenderer extends EntityRenderer<TomahawkProjectileEntity, EntityRenderState> {
+public class TomahawkProjectileRenderer extends EntityRenderer<TomahawkProjectileEntity,
+                                                TomahawkProjectileRenderState> {
     private final TomahawkProjectileModel model;
+    private TomahawkProjectileEntity entity;
+    private float partial;
 
     public TomahawkProjectileRenderer(EntityRendererProvider.Context context) {
         super(context);
@@ -22,14 +26,34 @@ public class TomahawkProjectileRenderer extends EntityRenderer<TomahawkProjectil
     }
 
     @Override
-    public @NotNull EntityRenderState createRenderState() {
-        return new EntityRenderState();
+    public @NotNull TomahawkProjectileRenderState createRenderState() {
+        return new TomahawkProjectileRenderState();
     }
 
     @Override
-    public void render(@NotNull EntityRenderState state, PoseStack poseStack,
+    public void extractRenderState(@NotNull TomahawkProjectileEntity entity,
+                                   @NotNull TomahawkProjectileRenderState state, float partialTick) {
+        super.extractRenderState(entity, state, partialTick);
+        this.entity = entity;
+        this.partial = partialTick;
+        state.xRot = entity.getXRot(partialTick);
+        state.yRot = entity.getYRot(partialTick);
+    }
+
+    @Override
+    public void render(@NotNull TomahawkProjectileRenderState state, @NotNull PoseStack poseStack,
                        @NotNull MultiBufferSource buffer, int packedLight) {
         poseStack.pushPose();
+        if (!entity.onGround()) {
+            poseStack.mulPose(Axis.YP.rotationDegrees(Mth.lerp(partial, entity.yRotO, entity.getYRot())));
+            poseStack.mulPose(Axis.XP.rotationDegrees(entity.getRenderingRotation() * 5f));
+            poseStack.translate(0, -1.0f, 0);
+        }
+        else {
+            poseStack.mulPose(Axis.YP.rotationDegrees(entity.groundedOffset.y));
+            poseStack.mulPose(Axis.XP.rotationDegrees(entity.groundedOffset.x));
+            poseStack.translate(0, -1.0f, 0);
+        }
         VertexConsumer vertexconsumer =
               ItemRenderer.getFoilBuffer(buffer, this.model.renderType(this.getTextureLocation()),false, false);
         this.model.renderToBuffer(poseStack, vertexconsumer, packedLight, OverlayTexture.NO_OVERLAY);
