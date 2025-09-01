106f0464b7651da7f23126316adec198ed49a0ae
karen matsunaga <Karen.f.matsunaga@gmail.com>
2025-09-01 15:25:09 -0300

    2025-09-01 Added Alexandrite armor and tool advancements

--- src/main/java/net/karen/mccoursemod/datagen/ModAdvancementProvider.java
+++ src/main/java/net/karen/mccoursemod/datagen/ModAdvancementProvider.java
@@ -4,6 +4,7 @@ import net.karen.mccoursemod.MccourseMod;
 import net.karen.mccoursemod.item.ModItems;
 import net.minecraft.advancements.Advancement;
 import net.minecraft.advancements.AdvancementHolder;
+import net.minecraft.advancements.AdvancementRequirements;
 import net.minecraft.advancements.AdvancementType;
 import net.minecraft.advancements.critereon.InventoryChangeTrigger;
 import net.minecraft.core.HolderLookup;
@@ -27,32 +28,89 @@ public class ModAdvancementProvider extends AdvancementProvider {
         @Override
         public void generate(HolderLookup.@NotNull Provider provider,
                              @NotNull Consumer<AdvancementHolder> consumer) {
-            // Adding all custom advancement
-            // ALEXANDRITE advancement
+            // Adding all custom ADVANCEMENTS
+            // ** Mccourse Mod root advancement -> ALEXANDRITE **
             AdvancementHolder rootAdvancement =
                 Advancement.Builder.advancement()
                            .display(ModItems.ALEXANDRITE.get(),
-                            Component.literal("Alexandrite"),
-                            Component.literal("The Power lies in the Alexandrite!"),
-                            ResourceLocation.fromNamespaceAndPath(MccourseMod.MOD_ID, "textures/block/alexandrite_ore.png"),
-                            AdvancementType.TASK, true, true, false)
+                                    Component.literal("Mccourse Mod"),
+                                    Component.literal("Collect all exclusive ores, armors, tools, etc. on your journey!"
+                                                      + " The Power lies in the Alexandrite!"),
+                                    ResourceLocation.fromNamespaceAndPath(MccourseMod.MOD_ID,
+                                                                          "textures/item/alexandrite.png"),
+                                    AdvancementType.TASK, true, true, false)
                            .addCriterion("has_alexandrite",
                                          InventoryChangeTrigger.TriggerInstance.hasItems(ModItems.ALEXANDRITE.get()))
-                           // Alexandrite advancement
-                           .save(consumer, ResourceLocation.fromNamespaceAndPath(MccourseMod.MOD_ID, "alexandrite"));
+                           .save(consumer, ResourceLocation.fromNamespaceAndPath(MccourseMod.MOD_ID, "root"));
 
-            // METAL DETECTOR advancement
+            // ** CUSTOM items advancement -> METAL DETECTOR **
             AdvancementHolder metalDetector =
                 Advancement.Builder.advancement()
                            .parent(rootAdvancement)
                            .display(ModItems.METAL_DETECTOR.get(),
-                            Component.literal("Metal Detector"),
-                            Component.literal("Batteries not included! (Actually doesn't need batteries)"),
-                            null, AdvancementType.TASK, true, true, false)
+                                    Component.literal("Metal Detector"),
+                                    Component.literal("Batteries not included! (Actually doesn't need batteries)"),
+                                    null, AdvancementType.TASK, true, true, false)
                            .addCriterion("has_metal_detector",
                                          InventoryChangeTrigger.TriggerInstance.hasItems(ModItems.METAL_DETECTOR.get()))
-                           // Metal Detector advancement
                            .save(consumer, ResourceLocation.fromNamespaceAndPath(MccourseMod.MOD_ID, "metal_detector"));
+
+            // ** CUSTOM tools advancement **
+            // ** CUSTOM VANILLA TOOLS -> Sword, Pickaxe, Shovel, Axe, Hoe, etc. **
+            AdvancementHolder alexandriteVanillaTools =
+                Advancement.Builder.advancement()
+                           .parent(rootAdvancement)
+                           .display(ModItems.ALEXANDRITE_PICKAXE.get(),
+                                    Component.literal("Alexandrite Vanilla Tools"),
+                                    Component.literal("Collect all exclusive Alexandrite vanilla tools!"),
+                                    null, AdvancementType.TASK, true, true, false)
+                           .addCriterion("has_alexandrite_pickaxe",
+                                         InventoryChangeTrigger.TriggerInstance.hasItems(ModItems.ALEXANDRITE_PICKAXE.get()))
+                           .addCriterion("has_alexandrite_axe",
+                                         InventoryChangeTrigger.TriggerInstance.hasItems(ModItems.ALEXANDRITE_AXE.get()))
+                           .addCriterion("has_alexandrite_sword",
+                                         InventoryChangeTrigger.TriggerInstance.hasItems(ModItems.ALEXANDRITE_SWORD.get()))
+                           .addCriterion("has_alexandrite_shovel",
+                                         InventoryChangeTrigger.TriggerInstance.hasItems(ModItems.ALEXANDRITE_SHOVEL.get()))
+                           .addCriterion("has_alexandrite_hoe",
+                                         InventoryChangeTrigger.TriggerInstance.hasItems(ModItems.ALEXANDRITE_HOE.get()))
+                           .requirements(AdvancementRequirements.Strategy.AND)
+                           .save(consumer, ResourceLocation.fromNamespaceAndPath(MccourseMod.MOD_ID,
+                                                                                 "alexandrite_vanilla_tools"));
+
+            // ** CUSTOM TOOLS -> Hammer, Paxel, etc. **
+            AdvancementHolder alexandriteCustomTools =
+                Advancement.Builder.advancement()
+                           .parent(alexandriteVanillaTools)
+                           .display(ModItems.ALEXANDRITE_HAMMER.get(),
+                                    Component.literal("Alexandrite Custom Tools"),
+                                    Component.literal("Collect all exclusive Alexandrite custom tools!"),
+                                    null, AdvancementType.TASK, true, true, false)
+                           .addCriterion("has_alexandrite_hammer",
+                                         InventoryChangeTrigger.TriggerInstance.hasItems(ModItems.ALEXANDRITE_HAMMER.get()))
+                           .requirements(AdvancementRequirements.Strategy.AND)
+                           .save(consumer, ResourceLocation.fromNamespaceAndPath(MccourseMod.MOD_ID,
+                                                                                 "alexandrite_custom_tools"));
+
+            // ** CUSTOM ARMORS -> Helmet, Chestplate, Leggings, Boots or Elytra **
+            AdvancementHolder alexandriteCustomArmors =
+                Advancement.Builder.advancement()
+                           .parent(rootAdvancement)
+                           .display(ModItems.ALEXANDRITE_CHESTPLATE.get(),
+                                    Component.literal("Alexandrite Custom Armors"),
+                                    Component.literal("Collect all exclusive Alexandrite custom armors!"),
+                                    null, AdvancementType.TASK, true, true, false)
+                           .addCriterion("has_alexandrite_helmet",
+                                         InventoryChangeTrigger.TriggerInstance.hasItems(ModItems.ALEXANDRITE_HELMET.get()))
+                           .addCriterion("has_alexandrite_chestplate",
+                                         InventoryChangeTrigger.TriggerInstance.hasItems(ModItems.ALEXANDRITE_CHESTPLATE.get()))
+                           .addCriterion("has_alexandrite_leggings",
+                                         InventoryChangeTrigger.TriggerInstance.hasItems(ModItems.ALEXANDRITE_LEGGINGS.get()))
+                           .addCriterion("has_alexandrite_boots",
+                                         InventoryChangeTrigger.TriggerInstance.hasItems(ModItems.ALEXANDRITE_BOOTS.get()))
+                           .requirements(AdvancementRequirements.Strategy.AND)
+                           .save(consumer, ResourceLocation.fromNamespaceAndPath(MccourseMod.MOD_ID,
+                                                                                 "alexandrite_custom_armors"));
         }
     }
 }
